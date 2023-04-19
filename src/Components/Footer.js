55ee4e3ea7ec450f9bd1fd88ec3d7e2cd9288713
jcannon04 <123973436+jcannon04@users.noreply.github.com>
2023-04-18 19:34:51 -0500

    submit course 301 lab 02

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,3 +1,14 @@
+// Bootstrap Components 
+import Container from 'react-bootstrap/Container'
+
+/**
+ * Footer component contains author of document.
+ * Used for global site footer
+ */
 export default function Footer({ author }) {
-  return <div>Author: {author}</div>
+  return (
+    <Container>
+      <p className='text-center'>Author: {author}</p>
+    </Container>
+  )
 }
