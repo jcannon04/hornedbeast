1e48d121134432efe8d7547c6f9d25d622bc416c
jcannon04 <123973436+jcannon04@users.noreply.github.com>
2023-04-19 23:39:43 -0500

    submit lab04 course 301

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,34 +1,43 @@
-import { useState } from 'react'
+import { useState } from "react"
 
 // Components
-import HornedBeast from './HornedBeast'
-import Search from './Search'
+import HornedBeast from "./HornedBeast"
+import Search from "./Search"
 
 // Bootstrap Components
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 
 /**
  * Main component contains all horned beast cards.
  * Creates all horned beast components based on object found in src/data.json
  */
 export default function Main({ beasts, selectBeast }) {
-  const [searchString, setSearchString] = useState('')
+  const [searchString, setSearchString] = useState("")
+  const [filterCriteria, setFilterCriteria] = useState(null)
 
   // remove white space lower case all chars in search string
   let searchCriteria = searchString.toLowerCase().trim()
 
-  // remove all beasts where either the keywords or title don't include search criteria
-  let filteredBeasts = beasts.filter(
-    (beast) =>
-      beast.title.toLowerCase().includes(searchCriteria) ||
-      beast.keyword.toLowerCase().includes(searchCriteria)
-  )
+  // filter beast for correct number of horns, and search is include in title or keyword
+  let filteredBeasts = beasts.filter((beast) => {
+
+    let hasCorrectHorns = true
+    if (filterCriteria) {
+      hasCorrectHorns = parseInt(filterCriteria) === beast.horns
+    }
+
+    return (
+      hasCorrectHorns &&
+      (beast.title.toLowerCase().includes(searchCriteria) ||
+        beast.keyword.toLowerCase().includes(searchCriteria))
+    )
+  })
 
   return (
     <Container>
-      <Search updateSearch={setSearchString} />
+      <Search updateSearch={setSearchString} updateFilter={setFilterCriteria} />
       <Row>
         {filteredBeasts.map((beast) => {
           return (
